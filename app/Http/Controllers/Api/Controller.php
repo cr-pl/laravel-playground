50617f6826c7429b0855c9f7f033d9af3b66de35
Cristian Plugaru <pcristian78@gmail.com>
2024-09-30 17:54:43 +0200

    API docs working

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+/**
+ * @OA\Info(title="API", version="0.1")
+ * @OA\PathItem(path="/api")
+ */
+abstract class Controller
+{
+    //
+}
--- routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::get('/users', 'UserController@index');
-Route::post('/users', 'UserController@store');
-Route::get('/users/{id}', 'UserController@show');
